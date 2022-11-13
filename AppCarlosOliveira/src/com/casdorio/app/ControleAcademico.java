8241f51d6fb206f96bf1e4117d94d85c6bfc84f8
casdorio <casdorio@gmail.com>
2022-11-13 15:48:00 -0300

    step 2 - variaveis

--- AppCarlosOliveira/src/com/casdorio/app/ControleAcademico.java
+++ AppCarlosOliveira/src/com/casdorio/app/ControleAcademico.java
@@ -1,8 +1,13 @@
 package com.casdorio.app;
 
 public class ControleAcademico {
+	
+	private static String[] nomes;
+	private static float[] av1;
+	private static float[] av2;
 
 	public static void main(String[] args){
 
+		
 	}	
 }
