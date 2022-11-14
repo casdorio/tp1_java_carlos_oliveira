778a4e363dad3b8464c2fd077dfedfbbc3be0aac
casdorio <casdorio@gmail.com>
2022-11-14 18:35:04 -0300

    step 4 - estruturas criadas

--- AppCarlosOliveira/src/com/casdorio/app/ControleAcademico.java
+++ AppCarlosOliveira/src/com/casdorio/app/ControleAcademico.java
@@ -9,13 +9,46 @@ public class ControleAcademico {
 	
 	private static final int TAMANHO = 100; 	
 	
+	private static String obterStatus(float media){		
+		if(media < 4) {
+			return "Reprovado";
+		}else if(media < 7){
+			return "Prova final";
+		}		
+		return "Aprovado";
+	}
+	
+	private static float media(float av1, float av2) {		
+		return (av1+av2)/2;
+	}
+	
+	private static void imprimir() {		
+		for (int i = 0; i < TAMANHO; i++) {
+			if(nomes[i] != null) {
+				imprimir(i);
+			}
+		}
+	}
+	private static void imprimir(int indice) {
+		
+		System.out.println("| "+indice+" | " + 
+							nomes[indice] + " | AV1: " + 
+							av1[indice] + " AV2: " + 
+							av2[indice] + " MEDIA: " +
+							media(av1[indice], av2[indice]) + " SITUAÇÃO: " +
+							obterStatus(media(av1[indice], av2[indice]))
+							);
+	}
 	public static void main(String[] args){
 
 		nomes = new String[TAMANHO];
 		av1 = new float[TAMANHO];
 		av2 = new float[TAMANHO];
+
 		Scanner in = new Scanner(System.in);
+		
 		String opcao;
+		int pos = 0;
 
 		do {
 			System.out.println("[1] Registrar as notas de um novo aluno");
@@ -24,32 +57,55 @@ public class ControleAcademico {
 			System.out.println("[4] Sair");
 			
 			System.out.print("Informe a opção desejada: ");						
-			opcao = in.next();					
-
+			opcao = in.next();
 			
 			switch (opcao) {
 			case "1":
-				System.out.println("Registrar as notas de um novo aluno: ");
+				if(pos < TAMANHO) {
+					System.out.print("Informe o nome do aluno: ");	
+					nomes[pos] = in.next();
+	
+					System.out.print("Informe a nota da AV1: ");	
+					av1[pos] = in.nextFloat();
+	
+					System.out.print("Informe a nota da AV2: ");	
+					av2[pos] = in.nextFloat();
+					
+					imprimir(pos);
+
+					pos++;
+				} else {
+					System.out.println("Tamanho maximo de alunos cadastrado atingido!");
+				}
 				break;
 
 			case "2":
-				System.out.println("Consultar boletim de um aluno: ");
+				System.out.print("Informe o numero do aluno: ");						
+				int id = in.nextInt();
+
+				if(id >= 0 && id < pos) {
+					imprimir(id);
+				} else {
+					System.out.println("O índice ["+id+"] é inválido!!!");
+				}
 				break;
 
 			case "3":
-				System.out.println("Consultar notas da turma: ");
+				imprimir();
 				break;
 
 			case "4":
-				System.out.println("sair: ");
+				System.out.println("Saída");
 				break;
 
 			default:
-				System.out.println("nem uma");
+				System.out.println("A opção ["+opcao+"] inválida");
 				break;
 			}
 		} while (!"4".equalsIgnoreCase(opcao));
 				
+		System.out.println("Processamento finalizado!!!");
+		
 		in.close();
 		
 		
